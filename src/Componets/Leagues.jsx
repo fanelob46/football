525d5d4ec2ff465cc75b1f6398993077f3af6cc2
fanelob46 <fanelob46@gmail.com>
2024-08-21 13:48:24 +0200

    fetching data from api

--- src/Componets/Leagues.jsx
+++ src/Componets/Leagues.jsx
@@ -1,29 +1,31 @@
 import React,{useState,useEffect} from 'react'
 import LeagueListing from './LeagueListing';
+import axios from 'axios';
 
 const Leagues = () => {
-    const [league, setLeagues] = useState([]);
-    console.log(league);
+    const [data, setData] = useState([]); //state that stors fetched data
+    
 
    useEffect(() => {
     const fetchTeams = async () => {
-        const url = 'https://api-football-v1.p.rapidapi.com/v3/leagues';
-        const options = {
-            method: 'GET',
-            headers: {
-                'x-rapidapi-key': '2503c33039msh8f40da65d80bf75p12f058jsn52c9fe118d3e',
-                'x-rapidapi-host': 'api-football-v1.p.rapidapi.com'
-            }
-        };
-        
-        try {
-            const response = await fetch(url, options);
-            const result = await response.text();
-            setLeagues(result);
-            console.log(result);
-        } catch (error) {
-            console.error(error);
-        }
+      const url = 'https://api-football-v1.p.rapidapi.com/v3/leagues';
+const options = {
+	method: 'GET',
+	headers: {
+		'x-rapidapi-key': '2503c33039msh8f40da65d80bf75p12f058jsn52c9fe118d3e',
+		'x-rapidapi-host': 'api-football-v1.p.rapidapi.com'
+	}
+};
+
+try {
+	const response = await fetch(url, options);
+	const result = await response.json();
+  setData(result.response)
+	console.log(result.response);
+  console.log(result.response[5]?.league?.name);
+} catch (error) {
+	console.error(error);
+}
         
         
       }
@@ -34,11 +36,22 @@ const Leagues = () => {
 
 
   return (
-    <div>
-        {league.map((team) => {
-         <LeagueListing key={team.id} team={team}/>
-        })}
+    <>
+<div>
+  {
+    data.map((leg) => 
+      < >
+    
+      <h1> {leg?.league?.name}</h1>
+      <img  src={leg?.league?.logo}  className='w-20 h-20'/>
+      </>
+    )
+  }
+     
+      
     </div>
+    </>
+    
    
   )
 }
