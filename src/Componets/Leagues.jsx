82ea78851150e19e61361047a0b655d076a5b3c5
fanelob46 <fanelob46@gmail.com>
2024-09-10 12:20:31 +0200

    responsiveness in all devices

--- src/Componets/Leagues.jsx
+++ src/Componets/Leagues.jsx
@@ -62,8 +62,8 @@ try {
 }).map((leg) => 
   < >
 
-  <div key={leg.id} leg={leg}>
-  <img  src={leg?.league?.logo}  className='w-20 h-20'/>
+  <div key={leg.id} leg={leg} className='kobus:py-10'>
+  <img  src={leg?.league?.logo}  className='w-20 h-20 kobus:w-[150px] kobus:h-[200px] '/>
   <h1> {leg?.league?.name}</h1>
   
   </div>
@@ -104,37 +104,41 @@ setPageNumber(selected);
    <div className="w-72 pt-7 pb-7 pl-7">
   <div className="relative w-full min-w-[200px] h-10">
     <input
-      className="peer w-full h-full bg-transparent text-[#2b2eff] font-sans font-normal outline outline-0 focus:outline-0 disabled:bg-[#2b2eff] disabled:border-0 transition-all  placeholder-shown:border-[#2b2eff] placeholder-shown:border-t-[#2b2eff] border focus:border-2 border-t-transparent focus:border-t-transparent text-sm px-3 py-2.5 rounded-[7px] border-blue-gray-200 focus:border-[#2b2eff]"
+      className="peer w-full kobus:py-6 h-full bg-transparent text-[#2b2eff] font-sans font-normal outline outline-0 focus:outline-0 disabled:bg-[#2b2eff] disabled:border-0 transition-all  placeholder-shown:border-[#2b2eff] placeholder-shown:border-t-[#2b2eff] border focus:border-2 border-t-transparent focus:border-t-transparent text-sm px-3 py-2.5 rounded-[7px] border-blue-gray-200 focus:border-[#2b2eff]"
       placeholder=" " onChange={(e) => SetSearch(e.target.value)}/><label
-      className="flex w-full h-full select-none pointer-events-none absolute left-0 font-normal !overflow-visible truncate peer-placeholder-shown:text-[#2b2eff] leading-tight peer-focus:leading-tight peer-disabled:text-transparent peer-disabled:peer-placeholder-shown:text-[#2b2eff] transition-all -top-1.5 peer-placeholder-shown:text-sm text-[11px] peer-focus:text-[11px] before:content[' '] before:block before:box-border before:w-2.5 before:h-1.5 before:mt-[6.5px] before:mr-1 peer-placeholder-shown:before:border-transparent before:rounded-tl-md before:border-t peer-focus:before:border-t-2 before:border-l peer-focus:before:border-l-2 before:pointer-events-none before:transition-all peer-disabled:before:border-transparent after:content[' '] after:block after:flex-grow after:box-border after:w-2.5 after:h-1.5 after:mt-[6.5px] after:ml-1 peer-placeholder-shown:after:border-transparent after:rounded-tr-md after:border-t peer-focus:after:border-t-2 after:border-r peer-focus:after:border-r-2 after:pointer-events-none after:transition-all peer-disabled:after:border-transparent peer-placeholder-shown:leading-[3.75] text-[#2b2eff] peer-focus:text-[#2b2eff] before:border-[#2b2eff] peer-focus:before:!border-[#2b2eff] after:border-[#2b2eff] peer-focus:after:!border-[#2b2eff]">Search League
+      className="flex w-full kobus:pt-1 h-full select-none pointer-events-none absolute left-0 font-normal !overflow-visible truncate peer-placeholder-shown:text-[#2b2eff] leading-tight peer-focus:leading-tight peer-disabled:text-transparent peer-disabled:peer-placeholder-shown:text-[#2b2eff] transition-all -top-1.5 peer-placeholder-shown:text-sm text-[11px] peer-focus:text-[11px] before:content[' '] before:block before:box-border before:w-2.5 before:h-1.5 before:mt-[6.5px] before:mr-1 peer-placeholder-shown:before:border-transparent before:rounded-tl-md before:border-t peer-focus:before:border-t-2 before:border-l peer-focus:before:border-l-2 before:pointer-events-none before:transition-all peer-disabled:before:border-transparent after:content[' '] after:block after:flex-grow after:box-border after:w-2.5 after:h-1.5 after:mt-[6.5px] after:ml-1 peer-placeholder-shown:after:border-transparent after:rounded-tr-md after:border-t peer-focus:after:border-t-2 after:border-r peer-focus:after:border-r-2 after:pointer-events-none after:transition-all peer-disabled:after:border-transparent peer-placeholder-shown:leading-[3.75] text-[#2b2eff] peer-focus:text-[#2b2eff] before:border-[#2b2eff] peer-focus:before:!border-[#2b2eff] after:border-[#2b2eff] peer-focus:after:!border-[#2b2eff]">Search League
     </label>
   </div>
 </div> 
-   <div className='flex justify-around'>
+   <div className='flex justify-around kobus:text-2xl'>
     <button className='bg-transparent  hover:bg-[#2b2eff] text-[#2b2eff] font-semibold hover:text-white py-2 px-4 border border-[#2b2eff] hover:border-transparent rounded' onClick={()=> setData(data)} >All Leagues</button>
     <button className='bg-transparent hover:bg-[#2b2eff] text-[#2b2eff] font-semibold hover:text-white py-2 px-4 border border-[#2b2eff] hover:border-transparent rounded' onClick={()=>filterItems("League")}>League</button>
     <button className='bg-transparent hover:bg-[#2b2eff] text-[#2b2eff] font-semibold hover:text-white py-2 px-4 border border-[#2b2eff] hover:border-transparent rounded' onClick={()=>filterItems("Cup")}>Cup</button>
    </div>
    
-<div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4 py-8 justify-items-center'>
+<div className='grid grid-cols-2 sm:grid-cols-2 md:grid-cols-4 gap-4  py-8 justify-items-center'>
   
   {
    displayLeagues
     
     
   }
-  <ReactPaginate 
+  <div className=''>
+  <ReactPaginate className='text-sm kobus:text-2xl flex place-items-center space-x-2 pl-36 sm:pl-80 sm:space-x-4 md:space-x-6 lg:space-x-8 md:pl-[580px] lg:pl-[800px] xl:pl-[1100px] pc:pl-[1400px] kobus:pl-[1850px]'
+
   
-  previousLabel={"Previous"}
+  previousLabel={"Previous " }
   nextLabel ={"Next"}
   pageCount={pageCount}
   onPageChange={changePage}
   containerClassName={"paginationBttns"}
-  previousLinkClassName={"previousBttn"}
-  nextLinkClassName={"nextBttn"}
+  previousLinkClassName={"previousBttn p-2 border border-[#2b2eff] rounded-md text-[#2b2eff]  transition"}
+  nextLinkClassName={"nextBttn p-2 border border-[#2b2eff] rounded-md text-[#2b2eff]  transition"}
   disabledClassName={"paginationDisabled"}
-  activeClassName={"paginationActive"}
+  activeClassName={"paginationActive p-2 border rounded-md text-white bg-[#2b2eff]"}
   />
+  </div>
+  
       
       
       
